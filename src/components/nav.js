f0f550397d05363b0f2119831cc5c1096c72db1e
Asela Madusanka <asela1madusanka@gmail.com>
2023-12-29 19:21:09 +0530

    update link

--- src/components/nav.js
+++ src/components/nav.js
@@ -208,7 +208,7 @@ const Nav = ({ isHome }) => {
   const ResumeLink = (
     <a
       className="resume-button"
-      href="https://drive.google.com/file/d/10VIoQuYw6sM2ANytIJEtzbnVnBRK56Ng/view?usp=sharing"
+      href="https://drive.google.com/file/d/1Q85763d2thyNbgAgPtKjEh2lWeMb14xW/view?usp=sharing"
       target="_blank"
       rel="noopener noreferrer">
       Resume
