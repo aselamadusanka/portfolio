b34c35768690cf42e1b8575168b9ecf242db0991
Asela Madusanka <asela1madusanka@gmail.com>
2024-03-28 11:53:47 +0530

    update cv link

--- src/components/nav.js
+++ src/components/nav.js
@@ -208,7 +208,7 @@ const Nav = ({ isHome }) => {
   const ResumeLink = (
     <a
       className="resume-button"
-      href="https://drive.google.com/file/d/1t_Qyow2Sw7LvgJa8Bm5BHjYTzKCo2xxt/view?usp=sharing"
+      href="https://drive.google.com/file/d/1XfG1Pwoa9nYn9H4vGMGelwC8608s8a2V/view?usp=sharing"
       target="_blank"
       rel="noopener noreferrer">
       Resume
